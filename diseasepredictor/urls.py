97b06e65f3a3103f7989dec911052e60869cad24
thearjun <mailarjunadhikari@gmail.com>
2019-10-10 00:35:25 +0545

    URL Routing Configuration

--- diseasepredictor/urls.py
+++ diseasepredictor/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('heart', views.heart, name="heart"),
+    path('diabetes', views.diabetes, name="diabetes"),
+    path('breast', views.breast, name="breast"),
+    path('', views.home, name="home"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
