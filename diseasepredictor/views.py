dcb9f99d7634eb1f39b5e000853db2544ed9f2ae
thearjun <mailarjunadhikari@gmail.com>
2019-10-10 00:34:39 +0545

    Models for Machine Learning and Backend Processing

---
+++ diseasepredictor/views.py
@@ -0,0 +1,192 @@
+from django.shortcuts import render
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def heart(request):
+    """ 
+    18:39:18 09 Oct, 2019 by Arjun Adhikari
+    Reading the training data set. 
+    """
+    df = pd.read_csv('static/Heart_train.csv')
+    data = df.values
+    X = data[:, :-1]
+    Y = data[:, -1:]
+
+    """ 
+    18:39:18 09 Oct, 2019 by Arjun Adhikari
+    Reading data from the user. 
+    """
+
+    value = ''
+
+    if request.method == 'POST':
+
+        age = float(request.POST['age'])
+        sex = float(request.POST['sex'])
+        cp = float(request.POST['cp'])
+        trestbps = float(request.POST['trestbps'])
+        chol = float(request.POST['chol'])
+        fbs = float(request.POST['fbs'])
+        restecg = float(request.POST['restecg'])
+        thalach = float(request.POST['thalach'])
+        exang = float(request.POST['exang'])
+        oldpeak = float(request.POST['oldpeak'])
+        slope = float(request.POST['slope'])
+        ca = float(request.POST['ca'])
+        thal = float(request.POST['thal'])
+
+        user_data = np.array(
+            (age,
+             sex,
+             cp,
+             trestbps,
+             chol,
+             fbs,
+             restecg,
+             thalach,
+             exang,
+             oldpeak,
+             slope,
+             ca,
+             thal)
+        ).reshape(1, 13)
+
+        rf = RandomForestClassifier(
+            n_estimators=16,
+            criterion='entropy',
+            max_depth=9
+        )
+
+        rf.fit(np.nan_to_num(X), Y)
+        rf.score(np.nan_to_num(X), Y)
+        predictions = rf.predict(user_data)
+
+        if int(predictions[0]) == 1:
+            value = 'have'
+        elif int(predictions[0]) == 0:
+            value = "don\'t have"
+
+    return render(request,
+                  'heart.html',
+                  {
+                      'context': value
+                  })
+
+
+def diabetes(request):
+    """ 
+    20:13:20 09 Oct, 2019 by Arjun Adhikari
+    Reading the training data set. 
+    """
+    dfx = pd.read_csv('static/Diabetes_XTrain.csv')
+    dfy = pd.read_csv('static/Diabetes_YTrain.csv')
+    X = dfx.values
+    Y = dfy.values
+    Y = Y.reshape((-1,))
+
+    """ 
+    20:18:20 09 Oct, 2019 by Arjun Adhikari
+    Reading data from user. 
+    """
+    value = ''
+    if request.method == 'POST':
+
+        pregnancies = float(request.POST['pregnancies'])
+        glucose = float(request.POST['glucose'])
+        bloodpressure = float(request.POST['bloodpressure'])
+        skinthickness = float(request.POST['skinthickness'])
+        bmi = float(request.POST['bmi'])
+        insulin = float(request.POST['insulin'])
+        pedigree = float(request.POST['pedigree'])
+        age = float(request.POST['age'])
+
+        user_data = np.array(
+            (pregnancies,
+             glucose,
+             bloodpressure,
+             skinthickness,
+             bmi,
+             insulin,
+             pedigree,
+             age)
+        ).reshape(1, 8)
+
+        knn = KNeighborsClassifier(n_neighbors=3)
+        knn.fit(X, Y)
+
+        predictions = knn.predict(user_data)
+
+        if int(predictions[0]) == 1:
+            value = 'have'
+        elif int(predictions[0]) == 0:
+            value = "don\'t have"
+
+    return render(request,
+                  'diabetes.html',
+                  {
+                      'context': value
+                  })
+
+
+def breast(request):
+    """ 
+    20:56:20 09 Oct, 2019 by Arjun Adhikari
+    Reading training data set. 
+    """
+    df = pd.read_csv('static/Breast_train.csv')
+    data = df.values
+    X = data[:, :-1]
+    Y = data[:, -1]
+    print(X.shape, Y.shape)
+
+    """ 
+    20:57:20 09 Oct, 2019 by Arjun Adhikari
+    Reading data from user. 
+    """
+    value = ''
+    if request.method == 'POST':
+
+        radius = float(request.POST['radius'])
+        texture = float(request.POST['texture'])
+        perimeter = float(request.POST['perimeter'])
+        area = float(request.POST['area'])
+        smoothness = float(request.POST['smoothness'])
+
+        """ 
+        21:02:21 09 Oct, 2019 by Arjun Adhikari
+        Creating our training model. 
+        """
+        rf = RandomForestClassifier(
+            n_estimators=16, criterion='entropy', max_depth=5)
+        rf.fit(np.nan_to_num(X), Y)
+
+        user_data = np.array(
+            (radius,
+             texture,
+             perimeter,
+             area,
+             smoothness)
+        ).reshape(1, 5)
+
+        predictions = rf.predict(user_data)
+        print(predictions)
+
+        if int(predictions[0]) == 1:
+            value = 'have'
+        elif int(predictions[0]) == 0:
+            value = "don\'t have"
+
+    return render(request,
+                  'breast.html',
+                  {
+                      'context': value
+                  })
+
+
+def home(request):
+
+    return render(request,
+                  'home.html')
